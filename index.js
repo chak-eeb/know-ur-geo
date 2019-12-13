fb1a7a9340f91f72ffe31aec03f6c532db617489
chak-eeb <chak_eeb@yahoo.com>
2019-12-13 10:58:30 -0500

    work on conditionals and counter...not done yet

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // start the game
 const startBtn = document.getElementById('start');
+const correct = document.querySelector('.correct');
+let counter = document.querySelector('.counter span').textContent;
+
 startBtn.addEventListener('click', start);
 const imagesArray = [
 	'../images/brazil.svg',
@@ -8,24 +11,85 @@ const imagesArray = [
 	'../images/morocco.png',
 	'../images/australia.jpeg'
 ];
-// const answer = document.getElementsById('value').value;
-// function checkAnswer(){
-// if(answer)
-// }
-// const submit = document.getElementsById('submit');
-// submit.addEventListener('click', checkAnswer);
-
-// function submit() {}
-
+// const china = '../images/china.jpg';
+// const brazil = '../images/brazil.svg';
+// const france = '../images/france.png';
+// const morocco = '../images/morocco.png';
+// const australia = '../images/australia.jpeg';
+let randomIndex = Math.floor(Math.random() * 5);
 function start() {
-	let randomIndex = Math.floor(Math.random() * 5);
 	console.log(randomIndex);
-	// const picture = imagesArray[randomIndex];
-	document.getElementById('pic').src = imagesArray[randomIndex];
 
-	// document.createElement('style').innerHTML =
-	// 	'.image {' +
-	// 	' background-image: ' +
-	// 	'url(../images/australia.jpeg); ' +
-	// 	'}';
+	document.getElementById('pic').src = imagesArray[randomIndex];
 }
+
+// submit function
+
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', answer);
+
+function answer() {
+	const inputValue = document.getElementById('val').value;
+	console.log(imagesArray[randomIndex]);
+	console.log(inputValue);
+	console.log(counter);
+
+	if (
+		inputValue == 'china' ||
+		imagesArray[randomIndex] == '../images/china.jpg'
+	) {
+		correct.textContent = 'CORRECT';
+		correct.style.color = 'green';
+		counter++;
+	} else {
+		correct.textContent = 'TRY AGAIN';
+		correct.style.color = 'red';
+	}
+	if (
+		inputValue == 'brazil' ||
+		imagesArray[randomIndex] == '../images/brazil.svg'
+	) {
+		correct.textContent = 'CORRECT';
+		correct.style.color = 'green';
+		counter++;
+	} else {
+		correct.textContent = 'TRY AGAIN';
+		correct.style.color = 'red';
+	}
+	if (
+		inputValue == 'france' ||
+		imagesArray[randomIndex] == '../images/france.png'
+	) {
+		correct.textContent = 'CORRECT';
+		correct.style.color = 'green';
+		counter++;
+	} else {
+		correct.textContent = 'TRY AGAIN';
+		correct.style.color = 'red';
+	}
+	if (
+		inputValue == 'morocco' ||
+		imagesArray[randomIndex] == '../images/morocco.png'
+	) {
+		correct.textContent = 'CORRECT';
+		correct.style.color = 'green';
+		counter++;
+	} else {
+		correct.textContent = 'TRY AGAIN';
+		correct.style.color = 'red';
+	}
+	if (
+		inputValue == 'australia' ||
+		imagesArray[randomIndex] == '../images/australia.jpeg'
+	) {
+		correct.textContent = 'CORRECT';
+		correct.style.color = 'green';
+		counter++;
+	} else {
+		correct.textContent = 'TRY AGAIN';
+		correct.style.color = 'red';
+	}
+}
+
+//
